d4bbdc4cd989c865846bcd7a314bf2eecec881a1
Howard Chen <csh7183@gmail.com>
2018-01-03 10:34:43 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+app.get("/",function(req,res){
+    res.render("index");
+});
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server has Started");
+});
